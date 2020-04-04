7ef1d1e8c0c4e1229677c38546162368218471cd
wijnandb <wb@retta.nl>
2020-04-04 20:11:57 +0200

    sommen in array

--- 0.6/script.js
+++ 0.6/script.js
@@ -31,24 +31,27 @@ document.getElementById("wrong").innerHTML = aantal_fout;
 
 // fill an array with sums
 var keersommen = [
-  [1,1],	[1,2],	[1,3],	[1,4],	[1,5],	[1,6],	[1,7],	[1,8],	[1,9],	[1,10],
-  [2,1],	[2,2],	[2,3],	[2,4],	[2,5],	[2,6],	[2,7],	[2,8],	[2,9],	[2,10],
-  [3,1],	[3,2],	[3,3],	[3,4],	[3,5],	[3,6],	[3,7],	[3,8],	[3,9],	[3,10],
-  [4,1],	[4,2],	[4,3],	[4,4],	[4,5],	[4,6],	[4,7],	[4,8],	[4,9],	[4,10],
-  [5,1],	[5,2],	[5,3],	[5,4],	[5,5],	[5,6],	[5,7],	[5,8],	[5,9],	[5,10],
-  [6,1],	[6,2],	[6,3],	[6,4],	[6,5],	[6,6],	[6,7],	[6,8],	[6,9],	[6,10],
-  [7,1],	[7,2],	[7,3],	[7,4],	[7,5],	[7,6],	[7,7],	[7,8],	[7,9],	[7,10],
-  [8,1],	[8,2],	[8,3],	[8,4],	[8,5],	[8,6],	[8,7],	[8,8],	[8,9],	[8,10],
-  [9,1],	[9,2],	[9,3],	[9,4],	[9,5],	[9,6],	[9,7],	[9,8],	[9,9],	[9,10],
-  [10,1],	[10,2],	[10,3],	[10,4],	[10,5],	[10,6],	[10,7],	[10,8],	[10,9],	[10,10]
+  [5,3],	[10,8],	[9,3],	[4,2],	[9,4],	[2,1],	[8,9],	[6,10],	[5,6],	[2,10],
+[3,3],	[4,8],	[7,3],	[1,2],	[7,4],	[8,1],	[6,9],	[5,9],	[3,6],	[8,10],
+[10,3],	[1,8],	[2,3],	[9,2],	[2,4],	[6,1],	[5,1],	[3,9],	[10,5],	[10,6],
+[4,3],	[9,8],	[8,3],	[7,2],	[8,4],	[5,4],	[3,1],	[10,9],	[4,5],	[4,6],
+[1,3],	[7,8],	[6,3],	[2,2],	[6,4],	[3,4],	[10,1],	[4,9],	[1,5],	[1,6],
+[5,7],	[2,8],	[9,7],	[8,2],	[5,2],	[10,4],	[4,1],	[1,9],	[9,5],	[9,6],
+[3,7],	[8,8],	[7,7],	[6,2],	[3,2],	[4,4],	[1,1],	[9,9],	[7,5],	[7,6],
+[10,7],	[6,8],	[2,7],	[5,8],	[10,2],	[1,4],	[9,1],	[7,9],	[2,5],	[2,6],
+[4,7],	[5,5],	[8,7],	[3,8],	[1,10],	[10,10],	[7,1],	[2,9],	[8,5],	[8,6],
+[1,7],	[3,5],	[6,7],	[7,10],	[9,10],	[4,10],	[3,10],	[5,10],	[6,5],	[6,6]
 ]
 
+
+
 function testSpeech() {
-  testBtn.disabled = true;
-  testBtn.textContent = '';
+  testBtn.disabled = false;
+  testBtn.textContent = 'stop';
 
   // get a random su from the array "keersommen"
-  randomsom = Math.floor(Math.random() * keersommen.length)
+  document.getElementById("total").innerHTML = aantal_sommen;
+  randomsom = aantal_sommen;
 
   times = keersommen[randomsom][0];
   toMult = keersommen[randomsom][1];
@@ -82,10 +85,10 @@ function testSpeech() {
     // We then return the transcript property of the SpeechRecognitionAlternative object 
     var speechResult = event.results[0][0].transcript;
     aantal_sommen += 1
-    document.getElementById("total").innerHTML = aantal_sommen;
+//    document.getElementById("total").innerHTML = aantal_sommen;
     resultPara.textContent = speechResult;
     if(parseInt(speechResult, 10) === phrase) {
-      delete keersommen[randomsom];      
+      delete keersommen[randomsom];
       tafels[times][toMult] = tafels[times][toMult] + 1;
       setColor(somvakje, tafels[times][toMult]);
       aantal_goed = aantal_goed + 1;
